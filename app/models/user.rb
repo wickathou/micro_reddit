2e8f94d411c9fe6fd80d62444525ed593b1cf119
wickathou <javidx@gmail.com>
2020-06-30 18:12:00 +0400

    Added comments to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :delete_all
+  has_many :comments
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
